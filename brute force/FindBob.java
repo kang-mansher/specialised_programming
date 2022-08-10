df4655bc34b0795a209864bb746a21a00d5d173d
Mansher Kang <kang@student-10-201-18-152.wireless.adelaide.edu.au>
2022-08-10 15:26:18 +0930

    added codes to files

--- brute force/FindBob.java
+++ brute force/FindBob.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class FindBob {
+import java.util.*;
+
+public class FindBob {
+
+    public static void main(String[] args) {
+
+        int[] n = {6,6,6,6,6,6,41,48};
+        int[][] arr = {
+                {-1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, 2, -1, -1},
+                {1, -1, -1, 2, -1, -1},
+                {-1, -1, -1, -1, 3, -1},
+                {-1, -1, -1, -1, 0, -1},
+                {5, -1, -1, -1, -1, 5},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, -1, 12,
+                        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, 42, -1, -1, -1, -1, -1, 36, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+        };
+        for(int i=0;i<n.length;i++) {
+            System.out.println(Arrays.toString(find(n[i], arr[i])));
+        }
+
+    }
+
+    public static int[] find(int N, int[] houses) {
+
+        List<Set<Integer>> list = new ArrayList<>();
+        boolean noHouseVisited = true;
+
+        for (int i = 0; i < N; i++) {
+            if (houses[i] != -1) {
+                noHouseVisited = false;
+                Set<Integer> set = new HashSet<>();
+                if(i-houses[i]>=0)
+                    set.add(i - houses[i]);
+                if(i+houses[i]<N)
+                    set.add(i + houses[i]);
+                list.add(set);
+            }
+        }
+
+        if (noHouseVisited) {
+            int[] result = new int[N];
+            for (int i = 0; i < N; i++) {
+                result[i] = i;
+            }
+            return result;
+        }
+
+        Set<Integer> finalSet = list.get(0);
+        list.forEach(finalSet::retainAll);
+
+        List<Integer> resultList = new ArrayList<>(finalSet);
+        Collections.sort(resultList);
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
+        }
+        return result;
+
+    }
+
 }
--- brute force/WordleColors.java
+++ brute force/WordleColors.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class WordleColors {
+public class WordleColors {
+
+    public static void main(String[] args) {
+
+        String[] list1 = {"BLOCK", "BLOCK", "BLURB", "ABBOT", "CANAL", "GREEN", "RATES"};
+        String[] list2 = {"BRICK", "BLOBS", "BOBBY", "BOBBY", "AAAAA", "BLACK", "STARE"};
+
+        for (int i = 0; i < list1.length; i++) {
+            System.out.println(color(list1[i], list2[i]));
+        }
+    }
+
+    public static String color(String hidden, String guess) {
+
+        int[] hiddenCharFreq = new int[26];
+        char[] result = {'-', '-', '-', '-', '-'};
+
+        for (int i = 0; i < 5; i++) {
+            if (guess.charAt(i) == hidden.charAt(i)) {
+                result[i] = 'g';
+            } else {
+                hiddenCharFreq[hidden.charAt(i) - 'A']++;
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (result[i] == '-' && hiddenCharFreq[guess.charAt(i) - 'A'] > 0) {
+                result[i] = 'y';
+                hiddenCharFreq[guess.charAt(i) - 'A']--;
+            }
+        }
+
+        StringBuilder str = new StringBuilder();
+        for (char c : result) {
+            str.append(c);
+        }
+        return str.toString();
+    }
 }
