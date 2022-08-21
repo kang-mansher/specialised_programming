284d61ea1b3816cca5c79f0cfc2e6ebd49c99699
Mansher Kang <mansher.sk@gmail.com>
2022-08-21 17:15:13 +0930

    modified logic

--- recursion/MyPow.java
+++ recursion/MyPow.java
@@ -8,7 +8,8 @@ public class MyPow {
             return x;
         }
         if(n<0) {
-            return (1 / x) * myPow(1 / x, -(n + 1));
+            x = 1/x;
+            n = -n;
         }
         return n % 2==0 ? myPow(x*x,n/2) : x * myPow(x*x, n/2);
     }
